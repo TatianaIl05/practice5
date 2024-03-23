66165872ea475fea37f7c2e97e5e7d903a205179
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-23 11:51:41 +0700

    Update task_10_5.py

--- task_10_5.py
+++ task_10_5.py
@@ -2,7 +2,7 @@ pin_code = int(input('Введите PIN-код: '))
 answer = 'OK'
 if 1900 <= pin_code <= 2050:
     answer = 'ERROR'
-if pin_code//1000 > 9 or pin_code//1000 == 0:
+if len(str(pin_code)) != 4:
     answer = 'ERROR'
 if len(str(pin_code)) != len(set(str(pin_code))):
     answer = 'ERROR'
