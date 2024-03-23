19fe0a7cfc8d4b8691f27914d8171d7ea9a6255b
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-03-23 11:46:30 +0700

    Update task_9_5.py

--- task_9_5.py
+++ task_9_5.py
@@ -15,5 +15,5 @@ print(max_tower, mid_tower, min_tower)
 # Another variant (without if)
 min_tower = min(min(tower_1, tower_2), tower_3)
 max_tower = max(max(tower_1, tower_2), tower_3)
-mid = tower_1 + tower_2 + tower_3 - min_tower - max_tower
+mid_tower = tower_1 + tower_2 + tower_3 - min_tower - max_tower
 print(max_tower, mid_tower, min_tower)
